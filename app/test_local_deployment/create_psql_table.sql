6aea30c590c50a7c042e259f9a20382ae2132487
jean.raisaro <jean.raisaro@epfl.ch>
2017-09-14 15:58:23 -0400

    sql script for postgres table

---
+++ app/test_local_deployment/create_psql_table.sql
@@ -0,0 +1,14 @@
+CREATE TABLE public.demo_data_encrypted
+(
+    location_cd character varying(50) COLLATE pg_catalog."default",
+    "time" character varying(7) COLLATE pg_catalog."default",
+    concept_cd character varying(50) COLLATE pg_catalog."default",
+    totalnum character varying(88) COLLATE pg_catalog."default"
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE public.demo_data_encrypted
+    OWNER to i2b2demodata;
